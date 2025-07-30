e68fece3400f1821f8d5dc72b75e08150f44f59f
水化 <168127639+suisanka@users.noreply.github.com>
2025-07-30 13:17:07 +0800

    kami

--- src/constants/mock.ts
+++ src/constants/mock.ts
@@ -1,11 +1,11 @@
 export const mock = {
-  title: 'ME Frp 幻缘映射',
-  subtitle: '免费 · 公益 · 好用 · 低延迟 · 稳定的内网穿透服务',
-  thirdtitle: '五年技术沉淀 · 稳定可靠',
+  title: 'KamiFrp 神人映射',
+  subtitle: '神人专业内网穿透服务',
+  thirdtitle: '自助研发 · 个人备案',
   stats: {
-    users: 50000,
-    nodes: 5000,
-    proxies: 50000,
+    users: 1145,
+    nodes: 114,
+    proxies: 114514,
     traffic: 11451.4 * 1024 * 1024 * 1024
   } as const,
 } as const
